4c5777e2a52ab8d7bd5f18ce1ab2acbf4184ab87
thegauraw <thegauraw@gmail.com>
2016-04-20 21:17:20 +1000

    Update seed to only run on development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,28 @@
 # basic sample data to start with
 
-Student.delete_all
+if Rails.env.development?
+  tables = ['students', 'progresses']
+  tables.each do |table|
+    # truncate sqlite
+    ActiveRecord::Base.connection.execute("DELETE FROM #{table};")
+    ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='#{table}';")
+  end
+  # Student.delete_all
 
-student1 = Student.create(first_name: 'Foo',
-  last_name: 'Bar'
-  )
+  student1 = Student.create(first_name: 'Foo',
+    last_name: 'Bar'
+    )
 
-student2 = Student.create(first_name: 'Baz',
-  last_name: 'Qux'
-  )
+  student2 = Student.create(first_name: 'Baz',
+    last_name: 'Qux'
+    )
 
-student1.progresses.create(lesson: 1, part:1)
-student1.progresses.create(lesson: 1, part:2)
+  student1.progresses.create(lesson: 1, part:1)
+  sleep(1)
+  student1.progresses.create(lesson: 1, part:2)
 
-student1.progresses.create(lesson: 1, part:1)
-student1.progresses.create(lesson: 1, part:2)
+  sleep(1)
+  student2.progresses.create(lesson: 1, part:1)
+  sleep(1)
+  student2.progresses.create(lesson: 1, part:2)
+end
