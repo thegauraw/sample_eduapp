667478d4aeaf5a6e8584cd214f14ed177d3b4dbb
thegauraw <thegauraw@gmail.com>
2016-04-20 16:19:50 +1000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eduapp::Application.config.secret_token = '98e360d048a2f668113584c30dd18feb75a315569f3f5ee9ee71b583c65d815e789c85b6053eecf86b6403605dc5718bf3782114520b2dcc2ddedf82b5f923ea'
